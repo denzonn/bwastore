bfba51fdb7cd2d9e6f9d010d74f39be3ea0184e0
Denzonn <denson753@gmail.com>
2022-10-23 15:32:38 +0800

    feat: category crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,25 +17,75 @@ use Illuminate\Support\Facades\Route;
 */
 
 //User Routes
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories');
-Route::get('/details/{id}', [App\Http\Controllers\DetailController::class, 'index'])->name('detail');
-Route::get('/cart', [App\Http\Controllers\CartController::class, 'index'])->name('cart');
-Route::get('/success', [App\Http\Controllers\CartController::class, 'success'])->name('success');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name(
+    'home'
+);
+Route::get('/categories', [
+    App\Http\Controllers\CategoryController::class,
+    'index',
+])->name('categories');
+Route::get('/details/{id}', [
+    App\Http\Controllers\DetailController::class,
+    'index',
+])->name('detail');
+Route::get('/cart', [
+    App\Http\Controllers\CartController::class,
+    'index',
+])->name('cart');
+Route::get('/success', [
+    App\Http\Controllers\CartController::class,
+    'success',
+])->name('success');
 
 //Register Routes
-Route::get('/register/success', [App\Http\Controllers\Auth\RegisterController::class, 'success'])->name('register-success');
+Route::get('/register/success', [
+    App\Http\Controllers\Auth\RegisterController::class,
+    'success',
+])->name('register-success');
 
 //Dashboard
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-Route::get('/dashboard/products', [App\Http\Controllers\DashboardProductController::class, 'index'])->name('dashboard-product');
-Route::get('/dashboard/products/create', [App\Http\Controllers\DashboardProductController::class, 'create'])->name('dashboard-product-create');
-Route::get('/dashboard/products/{id}', [App\Http\Controllers\DashboardProductController::class, 'details'])->name('dashboard-product-details');
+Route::get('/dashboard', [
+    App\Http\Controllers\DashboardController::class,
+    'index',
+])->name('dashboard');
+Route::get('/dashboard/products', [
+    App\Http\Controllers\DashboardProductController::class,
+    'index',
+])->name('dashboard-product');
+Route::get('/dashboard/products/create', [
+    App\Http\Controllers\DashboardProductController::class,
+    'create',
+])->name('dashboard-product-create');
+Route::get('/dashboard/products/{id}', [
+    App\Http\Controllers\DashboardProductController::class,
+    'details',
+])->name('dashboard-product-details');
 
+Route::get('/dashboard/transactions', [
+    App\Http\Controllers\TransactionController::class,
+    'index',
+])->name('dashboard-transaction');
+Route::get('/dashboard/transactions/{id}', [
+    App\Http\Controllers\TransactionController::class,
+    'details',
+])->name('dashboard-transaction-details');
+Route::get('/dashboard/settings', [
+    App\Http\Controllers\DashboardSettingController::class,
+    'store',
+])->name('dashboard-setting-store');
+Route::get('/dashboard/account', [
+    App\Http\Controllers\DashboardSettingController::class,
+    'account',
+])->name('dashboard-setting-account');
 
-Route::get('/dashboard/transactions', [App\Http\Controllers\TransactionController::class, 'index'])->name('dashboard-transaction');
-Route::get('/dashboard/transactions/{id}', [App\Http\Controllers\TransactionController::class, 'details'])->name('dashboard-transaction-details');
-Route::get('/dashboard/settings', [App\Http\Controllers\DashboardSettingController::class, 'store'])->name('dashboard-setting-store');
-Route::get('/dashboard/account', [App\Http\Controllers\DashboardSettingController::class, 'account'])->name('dashboard-setting-account');
+//Admin
+Route::prefix('admin')
+    // ->middleware(['auth', 'admin'])
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name(
+            'admin-dashboard'
+        );
+        Route::resource('category', CategoryController::class);
+    });
 
 Auth::routes();
