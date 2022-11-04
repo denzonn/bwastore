9ae4da9e57dba8ca3cfa909f0d26527be2d4f2d5
Denzonn <denson753@gmail.com>
2022-11-04 15:08:21 +0800

    feat: intergration dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionDetail;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('pages.dashboard');
+    public function index()
+    {
+        $transaction = TransactionDetail::with([
+            'transaction.user',
+            'product.galleries',
+        ])->whereHas('product', function ($product) {
+            $product->where('users_id', auth()->user()->id);
+        });
+
+        $revenue = $transaction->get()->reduce(function ($carry, $item) {
+            return $carry + $item->price;
+        });
+
+        $costumer = User::count();
+
+        return view('pages.dashboard', [
+            'transaction_count' => $transaction->count(),
+            'transaction_data' => $transaction->get(),
+            'revenue' => $revenue,
+            'costumer' => $costumer,
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,121 +1,73 @@
 @extends('layouts.dashboard')
 
 @section('title')
-  Store Dashboard
+Store Dashboard
 @endsection
 
 @section('content')
-    <div
-    class="section-content section-dashboard-home"
-    data-aos="fade-up"
-    >
+<div class="section-content section-dashboard-home" data-aos="fade-up">
     <div class="container-fluid">
-    <div class="dashboard-heading">
-        <div class="dashboard-title">Dashboard</div>
-        <p class="dashboard-subtitle">Look what you have made today!</p>
-    </div>
-    <div class="dashboard-content">
-        <div class="row">
-        <div class="col-md-4">
-            <div class="card mb-2">
-            <div class="card-body">
-                <div class="dashboard-card-title">Customer</div>
-                <div class="dashboard-card-subtitle">15,209</div>
-            </div>
-            </div>
+        <div class="dashboard-heading">
+            <div class="dashboard-title">Dashboard</div>
+            <p class="dashboard-subtitle">Look what you have made today!</p>
         </div>
-
-        <div class="col-md-4">
-            <div class="card mb-2">
-            <div class="card-body">
-                <div class="dashboard-card-title">Revenue</div>
-                <div class="dashboard-card-subtitle">$931,290</div>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card mb-2">
-            <div class="card-body">
-                <div class="dashboard-card-title">Transaction</div>
-                <div class="dashboard-card-subtitle">22,409,399</div>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="row mt-3">
-        <div class="col-12 mt-2">
-            <h5 class="mb-3">Recent Transactions</h5>
-            <a
-            href="dashboard-transactions-details.html"
-            class="card card-list d-block"
-            >
-            <div class="card-body">
-                <div class="row">
-                <div class="col-md-1">
-                    <img src="/images/dashboard-icon1.png" alt="" />
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">Customer</div>
+                            <div class="dashboard-card-subtitle">{{number_format($costumer)}}</div>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="col-md-4">Shirup Marzzan</div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">Revenue</div>
+                            <div class="dashboard-card-subtitle">Rp. {{number_format($revenue)}}</div>
+                        </div>
+                    </div>
+                </div>
 
-                <div class="col-md-3">Angga Risky</div>
-
-                <div class="col-md-3">12 Januari, 2020</div>
-
-                <col class="md-1 d-none d-md-block" />
-                <img src="/images/dashboard-arrow.svg" alt="" />
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">Transaction</div>
+                            <div class="dashboard-card-subtitle">{{number_format($transaction_count)}}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </a>
+            <div class="row mt-3">
+                <div class="col-12 mt-2">
+                    <h5 class="mb-3">Recent Transactions</h5>
+                    @foreach ($transaction_data as $transaction)
+                    <a href="{{route('dashboard-transaction-details', $transaction->id)}}"
+                        class="card card-list d-block">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-1">
+                                    <img src="{{Storage::url($transaction->product->galleries->first()->photos ?? '')}}"
+                                        class="w-75" />
+                                </div>
 
-            <a
-            href="dashboard-transactions-details.html"
-            class="card card-list d-block"
-            >
-            <div class="card-body">
-                <div class="row">
-                <div class="col-md-1">
-                    <img src="/images/dashboard-icon1.png" alt="" />
-                </div>
+                                <div class="col-md-4">{{$transaction->product->name ?? ''}}</div>
 
-                <div class="col-md-4">Shirup Marzzan</div>
+                                <div class="col-md-3">{{$transaction->transaction->user->name ?? ''}}</div>
 
-                <div class="col-md-3">Angga Risky</div>
+                                <div class="col-md-3">{{$transaction->created_at ?? ''}}</div>
 
-                <div class="col-md-3">12 Januari, 2020</div>
-
-                <col class="md-1 d-none d-md-block" />
-                <img src="/images/dashboard-arrow.svg" alt="" />
-                </div>
-            </div>
-            </a>
-
-            <a
-            href="dashboard-transactions-details.html"
-            class="card card-list d-block"
-            >
-            <div class="card-body">
-                <div class="row">
-                <div class="col-md-1">
-                    <img src="/images/dashboard-icon1.png" alt="" />
-                </div>
-
-                <div class="col-md-4">Shirup Marzzan</div>
-
-                <div class="col-md-3">Angga Risky</div>
-
-                <div class="col-md-3">12 Januari, 2020</div>
-
-                <col class="md-1 d-none d-md-block" />
-                <img src="/images/dashboard-arrow.svg" alt="" />
+                                <col class="md-1 d-none d-md-block" />
+                                <img src="/images/dashboard-arrow.svg" alt="" />
+                            </div>
+                        </div>
+                    </a>
+                    @endforeach
                 </div>
             </div>
-            </a>
-        </div>
         </div>
     </div>
-    </div>
-    </div>
+</div>
 @endsection
-
-
